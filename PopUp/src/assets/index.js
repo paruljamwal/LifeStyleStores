8091f41afe77184ede643d83628620e7f953b59d
paruljamwal <paruljamwal2000@gmail.com>
2022-05-03 17:59:23 +0530

    PopUp

---
+++ PopUp/src/assets/index.js
@@ -0,0 +1,10 @@
+
+
+export const productImages=[
+    require('./images/p1.jpg'),
+    require('./images/p2.jpg'),
+    require("./images/p3.jpg"),
+    require('./images/p4.jpg'),
+    require('./images/p5.jpg'),
+    require('./images/p6.jpg')
+]
